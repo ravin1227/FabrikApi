a5499963731a20607603f86914ae1c09e9d7d178
Ravindra Verma <64417680+ravin1227@users.noreply.github.com>
2022-08-26 15:34:15 +0530

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,13 +1,16 @@
 import multer from 'multer'
+import path from 'path'
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
         cb(null,'uploads')
     },
     filename:(req,file,cb)=>{
-        cb(null,file.originalname)
+        // cb(null,file.originalname)
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 });
 
 const upload = multer({storage:storage}); 
-export default  upload ;
+// const multipleUpload = upload.fields([{name: 'screenshot', maxCount: 1}, {name: 'object', maxCount:1}]);
+export default  upload;
