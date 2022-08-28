2f8e5a2929ccd35c463d6dc08160d7e4b232797e
Ravindra Verma <64417680+ravin1227@users.noreply.github.com>
2022-08-28 13:30:41 +0530

    Update post-controllers.js

--- controllers/post-controllers.js
+++ controllers/post-controllers.js
@@ -4,20 +4,12 @@ import moment from "moment";
 import fs from 'fs'
 
 export const getAllPost = async (req, res, next) =>{
-    let post;
 
-    try {
-        post = await Post.find().populate("name");
-
-    } catch (error) {
-        return console.log(error);
-    }
-
-    if(!post){
-        return res.status(404).json({message : "No post found"});
-    }
-
-    return res.status(200).json({post});
+    await Post.find().then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err)
+    })
     
 }
 
@@ -30,14 +22,9 @@ export const postItem = async (req,res,next) => {
         const post = new Post({
             name:name,
             title:title,
-            // screenshot: {
-            //     data: fs.readFileSync("uploads/" + req.file.filename),
-            //     contentType:"image/png"
-            // },
             object:{
                 data: fs.readFileSync("uploads/" + req.file.filename),
-               // contentType:"image/png",
-                contentType:"model/gltf-binary"
+                contentType:"image/png",
             },
             date: date
         });
@@ -48,25 +35,14 @@ export const postItem = async (req,res,next) => {
             res.status(401).json({error})
         }
     return res.status(200).json({post});
-
-//     if(req.files){
-//         console.log(req.files)
-//         console.log("file uploaded")
-//     }
 }
 
 export const getById = async (req, res, next) => {
     const postId = req.params.id;
-    let post;
-
-    try {
-        post = await Post.findById(postId);
-    } catch (error) {
-        return console.log(error);
-    }
-
-    if(!post){
-        return res.status(500).json({message: "Post not found"});
-    }
-    return res.status(200).json({post});
+    Post.findById(postId).then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err)
+    })
+  
 }
